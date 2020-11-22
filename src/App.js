57b34d8beb84519f933a48096a9b88ca96503df0
ohidujjamanfahad <ohidujjamanfahad987@gmail.com>
2020-11-23 00:22:52 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {BrowserRouter as Router } from 'react-router-dom'
+import { GlobalStyle } from './globalStyle';
+import Hero from './components/Hero';
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Feature from './components/Feature';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+    
+      <Hero />
+      <Products heading='Chose Your Meal' data={productData}/>
+      <Feature />
+      <Products heading='Sweet Treats for You' data={productDataTwo}/>
+      <Footer />
+      
+    
+    </Router>
   );
 }
 
